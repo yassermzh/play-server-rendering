3773fc7d9e4ff5372e866e739fcb790380248793
Yasser <yassermzh@gmail.com>
2016-12-03 14:15:27 +0330

    redner having redux and router

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom/server'
 import { match, RouterContext } from 'react-router'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+const store = createStore((state = {}, action) => ({ x: 1 }), {})
 
 const App = (props) => <div><h1>app</h1>{props.children}</div>
-const Page1 = (props) => <div>Page1! id={props.params.id}</div>
+const Page1 = connect((state) => state)(
+  props => <div>Page1! id={props.params.id}, x={props.x}</div>
+)
 const routes = {
   path: '/',
   component: App,
@@ -16,8 +22,9 @@ const routes = {
 match({ routes, location: '/page1/2?q=3' }, (error, redirectLocation, renderProps) => {
   if (error) { console.log('failed! error=', error); return }
   else {
-    // console.log('renderProps=', Object.keys(renderProps))
-    const rendered = ReactDom.renderToStaticMarkup(<RouterContext {...renderProps} />)
+    const rendered = ReactDom.renderToStaticMarkup(
+      <Provider store={store}><RouterContext {...renderProps} /></Provider>
+    )
     console.log('rendered=', rendered)
   }
 })
